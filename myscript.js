a34e08695837dc380568f10d1c2c31f6b9ed815d
Gen Tamura <gen.tamura84@gmail.com>
2016-01-25 09:46:19 +0000

    Update

--- myscript.js
+++ myscript.js
@@ -1,34 +1,26 @@
-console.log('twitter 5 now!');
-
-//要素の作成
-var div = document.createElement('div');
-div.textContent = 'hoge';
-
-/*
-//最後の子要素として追加
-element.appendChild(div);
-
-//最初の子要素として追加
-element.insertBefore(div, element.firstChild);
-
-//要素の直前に追加
-element.parentNode.insertBefore(div, element);
-
-//要素の直後に追加
-element.parentNode.insertBefore(div, element.nextSibling);
-*/
-
-var doms = document.getElementsByClassName('PollCreator');
-console.log('doms', doms);
-/*
-for (var p in doms) {
-  console.log('doms[p]', doms[p]);
-  if (doms[p].parentNode) {
-    // doms[p].parentNode.insertBefore(div, doms[p].nextSibling);
-    doms[p].parentNode.appendChild(div);
-    console.log('div', div);
-    console.log('doms[p].parentNode', doms[p].parentNode);
-    console.log('doms[p].parentNode.insertBefore', doms[p].parentNode.insertBefore);
+jQuery(function(){
+  var doms = document.getElementsByClassName('PollCreator');
+  var div;
+  for (var p in doms) {
+    div = createNode();
+    if (doms[p].parentNode) {
+      doms[p].parentNode.insertBefore(div, doms[p].nextSibling);
+    }
   }
-}
-*/
+
+  function createNode() {
+    var div = document.createElement('div');
+    var smile = twemoji.parse('\u263a', { size: 36 });
+    var node =
+      '<button class="btn icon-btn PollCreator-btn js-tooltip js-twemoji" type="button" title="絵文字を追加" data-delay="150" tabindex="0">' +
+        '<span class="Icon Icon--poll">' + smile + '</span>' +
+        '<span class="text">絵文字</span>' +
+      '</button>'
+    ;
+    div.classList.add('TwemojiCreater')
+    div.innerHTML = node;
+    return div;
+  }
+
+  console.log('Twemoji Extension for Twitter loaded!');
+});
